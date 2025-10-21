2f9f7277d4dd46714e48c883225c6e86e5bac072
Ahmed Abrar <abrarjubah82@gmail.com>
2025-10-21 19:19:35 +0600

    Image post

--- app/main.py
+++ app/main.py
@@ -1,7 +1,15 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, File, UploadFile, HTTPException
+from fastapi.responses import JSONResponse
 
 app = FastAPI()
 
 @app.get("/")
-def read_root():
+async def read_root():
     return {"Hello": "World"}
+
+@app.post("/fossil-image/")
+async def get_rag_output(
+    image_file: list[UploadFile] = File(...)
+):
+    print(f"Received file: {len(image_file)} files")
+
